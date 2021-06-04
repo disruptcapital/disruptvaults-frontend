bd02cf221be5222352d0132f4664d01dfca45624
disrupttechno <billconrad@protonmail.com>
2021-06-03 22:24:45 -0400

    base layout for vaults

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Vaults } from './components/Vaults'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="app" class="d-flex h-100 text-center text-white bg-dark">
+      <div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+        <header class="mb-auto">
+          <div>
+            <h3 class="float-md-start mb-0">Disrupt Vaults</h3>
+            <nav class="nav nav-masthead justify-content-center float-md-end">
+              <a class="nav-link" href="#">
+                Contact
+              </a>
+            </nav>
+          </div>
+        </header>
+
+        <main class="px-3">
+          <h1>Compounding...</h1>
+          <p class="lead">Binance Smart Chain yield aggregators with a fresh perspective.</p>
+          <Vaults />
+        </main>
+
+        <footer class="mt-auto text-white-50">
+          <p>
+            Yield Optimizing Strategies by{' '}
+            <a href="https://twitter.com/disrupttechno" class="text-white">
+              @disruptcapital
+            </a>
+            .
+          </p>
+        </footer>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
