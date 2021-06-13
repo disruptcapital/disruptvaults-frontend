8e8beac2e0a10b3278f580601a196fea16e957d0
disrupttechno <billconrad@protonmail.com>
2021-06-13 18:14:38 -0400

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,29 +1,20 @@
-import './App.css'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import { Vaults } from './components/Vaults'
+import 'bootstrap/scss/bootstrap.scss';
+import './App.scss';
+
+import { Vaults } from './components/Vaults';
+import { Header } from 'components/Header';
 
 function App() {
   return (
-    <div id="app" class="d-flex h-100 text-center text-white bg-dark">
-      <div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
-        <header class="mb-auto">
-          <div>
-            <h3 class="float-md-start mb-0">Disrupt Vaults</h3>
-            <nav class="nav nav-masthead justify-content-center float-md-end">
-              <a class="nav-link" href="#">
-                Contact
-              </a>
-            </nav>
-          </div>
-        </header>
-
-        <main class="px-3">
-          <h1>Compounding...</h1>
-          <p class="lead">Binance Smart Chain yield aggregators with a fresh perspective.</p>
+    <div id="app" className="h-100 text-light bg-dark">
+      <div class="container d-flex w-100 h-100 p-3 mx-auto flex-column">
+        <Header />
+        <main className="px-3 text-center">
+          <h2>Compounding...</h2>
+          <p>Binance Smart Chain yield aggregators with a fresh perspective.</p>
           <Vaults />
         </main>
-
-        <footer class="mt-auto text-white-50">
+        <footer class="mt-auto text-white-50 text-center">
           <p>
             Yield Optimizing Strategies by{' '}
             <a href="https://twitter.com/disrupttechno" class="text-white">
@@ -34,7 +25,7 @@ function App() {
         </footer>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
