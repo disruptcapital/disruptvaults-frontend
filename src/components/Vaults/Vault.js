c3e2f73a42528c4d611c8880980b4d13fe2fe844
darthg8r <darth@darth.com>
2021-06-18 17:20:46 -0500

    Vault display setup

---
+++ src/components/Vaults/Vault.js
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from 'react';
+import { Container } from 'react-bootstrap';
+import Accordion from 'react-bootstrap/Accordion';
+import Card from 'react-bootstrap/Card';
+import {getNetworkPools} from '../../helpers/getNetworkData';
+import {useConnectWallet} from '../../hooks/connectWallet';
+import {erc20ABI} from '../../configure/abi';
+
+const Vault = (props) => {
+  const { web3, address, networkId, connected, connectWalletPending }  = useConnectWallet();
+
+  return (
+      <Accordion>
+        <Card>
+          <Accordion.Toggle as={Card.Header} eventKey="1">
+            decimals: {decimals} <br />
+
+            
+            <pre>Name: {pool.name} <br/>
+            earnedTokenAddress: { pool.earnedTokenAddress }
+            </pre>
+          </Accordion.Toggle>
+          <Accordion.Collapse eventKey="1">
+            <Card.Body className="text-black">
+              <div class="row">
+                <div class="col-6">                  
+                  <input type="number" placeholder="Deposit Amount"></input>
+                  <button class="btn btn-primary" onClick={deposit}>Deposit</button>
+                  <button class="btn btn-secondary" onClick={depositAll}>Deposit All</button>
+                </div>
+                <div class="col-6">
+                  <input type="number" placeholder="Withdraw Amount"></input>
+                  <button class="btn btn-primary" onClick={withdraw}>Withdraw</button>
+                  <button class="btn btn-secondary" onClick={withdrawAll}>Withdraw All</button>                  
+                </div>
+              </div>
+            </Card.Body>
+          </Accordion.Collapse>
+        </Card>
+      </Accordion>
+
+
+};
+
+export default Vault;
--- src/configure/index.js
+++ src/configure/index.js
@@ -5,26 +5,26 @@ const {
   heco: hecoAddressBook,
   avax: avaxAddressBook,
   bsc: bscAddressBook,
-  fantom: fantomAddressBook,
+  testnet: testnetAddressBook,
+  fantom: fantomAddressBook
 } = addressBook;
-export { bscAddressBook, hecoAddressBook, avaxAddressBook, polygonAddressBook, fantomAddressBook };
+export { bscAddressBook, hecoAddressBook, avaxAddressBook, polygonAddressBook, fantomAddressBook, testnetAddressBook };
 
 export {
   vaultABI,
-  bnbVaultABI,
   erc20ABI,
   strategyABI,
   multicallABI,
-  govPoolABI,
-  beefyUniV2ZapABI,
   uniswapV2PairABI,
   uniswapV2RouterABI,
 } from './abi';
 export { bscStakePools } from './stake/bsc_stake';
+export { testnetStakePools } from './stake/testnet_stake';
 export { hecoStakePools } from './stake/heco_stake';
 export { avalancheStakePools } from './stake/avalanche_stake';
 export { polygonStakePools } from './stake/polygon_stake';
 export { fantomStakePools } from './stake/fantom_stake';
+export { testnetPools } from './vault/testnet_pools';
 export { bscPools } from './vault/bsc_pools';
 export { hecoPools } from './vault/heco_pools';
 export { avalanchePools } from './vault/avalanche_pools';
@@ -33,6 +33,7 @@ export { hecoZaps } from './zap/heco_zaps';
 export { avalancheZaps } from './zap/avalanche_zaps';
 export { polygonZaps } from './zap/polygon_zaps';
 export { fantomZaps } from './zap/fantom_zaps';
+export { testnetZaps } from './zap/testnet_zaps';
 export { nativeCoins } from './native_coins';
 export { polygonPools } from './vault/polygon_pools';
 export { fantomPools } from './vault/fantom_pools';
